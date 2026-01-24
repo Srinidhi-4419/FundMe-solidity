497362d88af16c2e935bfc114431fa9e011b40e8
Srinidhi Kulkarni <kulkarnisrinidhi85@gmail.com>
2026-01-24 08:51:11 +0000

    added more coverage to fundme

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -17,35 +17,116 @@ contract FundMeTest is Test {
         assertTrue(true);
     }
 
-    // function testMinimumUsd() public {
-    //     assertEq(fundMe.MINIMUM_USD(), 518);
-    // }
-    function testOwner() public{
-        console.log(msg.sender);
+    function testMinimumUsd() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwner() public {
         assertEq(fundMe.i_owner(), msg.sender);
     }
-    // how to work with addresses in test which are outisde of our control
-    // 1. fork mainnet / testnet- COMMAND-forge test --match-test testPriceFeedVersion --fork-url <RPC_URL>
-    // 2.uNIT TESTING
-    // 3. INTEGRATION TESTING
-    // 4. STAGING TESTING
 
-    function testPriceFeedVersion() public{
+    function testPriceFeedVersion() public {
         assertEq(fundMe.getVersion(), 4);
     }
-  function testFundUpdatesFundedDataStructure() public {
-    address user = address(1);
 
-    vm.deal(user, 10e18);
-    vm.prank(user);
+    function testFundUpdatesFundedDataStructure() public {
+        address user = address(1);
+        vm.deal(user, 10e18);
+        vm.prank(user);
 
-    fundMe.fund{value: 6e18}();
+        fundMe.fund{value: 6e18}();
 
-    uint256 amountFunded =
-        fundMe.getaddresstoamountfunded(user);
+        uint256 amountFunded =
+            fundMe.getaddresstoamountfunded(user);
 
-    assertEq(amountFunded, 6e18);
-}
+        assertEq(amountFunded, 6e18);
+    }
 
+    function testFundersToArray() public {
+        address user = address(1);
+        vm.deal(user, 10e18);
+        vm.prank(user);
 
+        fundMe.fund{value: 6e18}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, user);
+    }
+
+    modifier funded() {
+        address user = address(1);
+        vm.deal(user, 10e18);
+        vm.prank(user);
+        fundMe.fund{value: 6e18}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        address user = address(1);
+        vm.prank(user);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+    function testWithdrawWithASingleFunder() public funded {
+        // Arrange
+        uint256 startingFundMeBalance =
+            address(fundMe).balance;
+        uint256 startingOwnerBalance = address(this).balance;
+
+        // Act
+        vm.prank(fundMe.i_owner());
+        fundMe.withdraw();
+
+        // Assert
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        uint256 endingOwnerBalance = address(this).balance;
+
+        assertEq(endingFundMeBalance, 0);
+        // because of gas costs, can't assert equality
+        assertGt(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+    function testWithdrawWithMultipleFunders() public funded {
+        // Arrange
+        uint160 numberOfFunders = 10;
+        uint256 fundAmount = 6e18;
+
+        for (uint160 i = 1; i <= numberOfFunders; i++) {
+            address funder = address(i);
+            vm.deal(funder, 10e18);
+            vm.prank(funder);
+            fundMe.fund{value: fundAmount}();
+        }
+
+        uint256 startingFundMeBalance =
+            address(fundMe).balance;
+        uint256 startingOwnerBalance = address(this).balance;
+
+        // Act
+        vm.prank(fundMe.i_owner());
+        fundMe.withdraw();
+
+        // Assert
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        uint256 endingOwnerBalance = address(this).balance;
+
+        assertEq(endingFundMeBalance, 0);
+        // because of gas costs, can't assert equality
+        assertGt(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+
+        // Make sure the funders are reset properly
+        vm.expectRevert();
+        fundMe.getFunder(0);
+        for (uint160 i = 1; i <= numberOfFunders; i++) {
+            address funder = address(i);
+            uint256 amountFunded =
+                fundMe.getaddresstoamountfunded(funder);
+            assertEq(amountFunded, 0);
+        }
+    }
 }
